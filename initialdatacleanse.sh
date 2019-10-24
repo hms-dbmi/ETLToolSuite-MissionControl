03beb09574dec16af61f254e3ff3af7c8d11b8fb
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2019-10-24 12:15:06 -0400

    update

--- initialdatacleanse.sh
+++ initialdatacleanse.sh
@@ -60,12 +60,12 @@ aws s3 cp s3://stage-$1-etl/resources/job.config resources/job.config
 
 java -jar DataEvaluation.jar -propertiesfile resources/job.config
 
-aws s3 cp resources/dataevaluation.txt s3://stage-general-etl/$1_dataevaluation.txt
+aws s3 cp resources/dataevaluation.txt s3://stage-general-etl/data_evaluations/$1_dataevaluation.txt
 
 # Clean up dirs
-rm -rf mappings/mapping.csv
-rm -rf mappings/mapping.csv.patient
-rm -rf completed/*
-rm -rf data/*
-rm -rf dict/*
-rm -rf processing/*
+#rm -rf mappings/mapping.csv
+#rm -rf mappings/mapping.csv.patient
+#rm -rf completed/*
+#rm -rf data/*
+#rm -rf dict/*
+#rm -rf processing/*
