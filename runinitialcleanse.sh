acb8041ae1e6ba6050278efaec64ca239b6d266f
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2019-10-25 12:41:23 -0400

    fdfa

--- runinitialcleanse.sh
+++ runinitialcleanse.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-studyids=("aric")
-studyids2=()
+studyids=("mesa" "mghaf" "partners" "safs" "sage" "sarcoidosis" "sas" "thrv" "vafar" "vuaf" "wghs" "whi" "hvh" "jhs" "mayovte")
+#studyids=("aric" "bags" "ccaf" "cfs" "chs" "copdgene" "cra" "dhs" "eocopd" "galaii" "genestar" "genoa" "gensalt" "goldn" "hchs" "hrmn" "hvh" "hypergen" "jhs"  
+studyids2=("fhs")
 
 for studyid in ${studyids[@]}; do
 	bash initialdatacleanse.sh $studyid
+
+	while [ $(ps aux | grep EntityGenerator.jar | wc -l) -gt 0 ]; do
+        sleep 2
+    done	
 done
 
 for studyid in ${studyids2[@]}; do
 	bash initialdatacleanse.sh $studyid "Y"
+	while [ $(ps aux | grep EntityGenerator.jar | wc -l) -gt 0 ]; do
+        sleep 2
+    done
 done
