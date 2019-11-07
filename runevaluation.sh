79ca9bd68e6b99b3d26c304e1f39a2c19d8366f5
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2019-11-07 18:15:44 -0500

    sdfad

--- runevaluation.sh
+++ runevaluation.sh
@@ -9,12 +9,12 @@ studyids=("fhs" "jhs" "mesa" "copdgene")
 
 for studyid in ${studyids[@]}; do
 
-	aws s3 cp s3://stage-$1-etl/data/ data/ --recursive
-	aws s3 cp s3://stage-$1-etl/mapping/mapping.csv mappings/mapping.csv
-	aws s3 cp s3://stage-$1-etl/resources/job.config resources/job.config
+	aws s3 cp s3://stage-${studyid}-etl/data/ data/ --recursive
+	aws s3 cp s3://stage-${studyid}-etl/mapping/mapping.csv mappings/mapping.csv
+	aws s3 cp s3://stage-${studyid}-etl/resources/job.config resources/job.config
 
 	java -jar DataEvaluation.jar -propertiesfile resources/job.config
 
-	aws s3 cp resources/dataevaluation.txt s3://stage-general-etl/$1_dataevaluation.txt
+	aws s3 cp resources/dataevaluation.txt s3://stage-general-etl/${studyid}_dataevaluation.txt
 
 done
