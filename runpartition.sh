269a5f9a3b875bdc6bdc3fc0dabfed0ff8980a88
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2020-04-15 16:42:08 -0400

    update to script

--- runpartition.sh
+++ runpartition.sh
@@ -13,7 +13,17 @@ done
 
 for filename in ${resdir}${configfile}; do
 
-        java -jar DataAnalyzer.jar -propertiesfile $filename -Xmx${memory} > /var/logs/${configfile}.log
+        nohup java -jar DataAnalyzer.jar -propertiesfile $filename -Xmx${memory} > /var/logs/${configfile}.log &
+
+        if [ $(ps aux | grep DataAnalyzer.jar | wc -l) -gt ${maxjobs} ]
+           then
+                sleep 5
+        fi
+
+done
+
+for filename in ${resdir}${configfile}; do
+
         nohup java -jar GenerateAllConcepts.jar -propertiesfile $filename -Xmx${memory} >> /var/logs/${configfile}.log & 
 
         if [ $(ps aux | grep GenerateAllConcepts.jar | wc -l) -gt ${maxjobs} ]
